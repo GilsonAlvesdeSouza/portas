2aaa53d555c97ce4388ea17f4bb81e90b57443af
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2021-08-31 01:09:43 -0300

    porta parte 1

---
+++ components/Porta.jsx
@@ -0,0 +1,9 @@
+import {Numero, PortaDiv} from "../styles/Porta";
+
+export default function Porta(props) {
+    return (
+        <PortaDiv>
+            <Numero>3</Numero>
+        </PortaDiv>
+    );
+}
---
+++ styles/Porta.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const PortaDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 200px;
+  height: 310px;
+  background-color: chocolate;
+  padding: 15px;
+`;
+
+export const Numero = styled.div`
+  font-size: 3rem;
+`;
